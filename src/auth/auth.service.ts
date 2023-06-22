9800c6859d5840ee2b382ffb060b9ec6701e6ca7
Cristian Morales <crisdavidmm95.dev@gmail.com>
2023-06-21 19:25:53 -0500

    add logic to signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -35,7 +35,20 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { message: 'I am signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Invalid credentials');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Invalid credentials');
+
+    delete user.hash;
+    return user;
   }
 }
