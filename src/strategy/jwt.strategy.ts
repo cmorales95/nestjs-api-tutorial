35b0c56aee3d15f4df9b23c2d4f70730861a2be6
Cristian Morales <crisdavidmm95.dev@gmail.com>
2023-06-25 23:50:33 -0500

    feat: add jwt guard for an endpoint and create custom guard
    Signed-off-by: Cristian Morales <crisdavidmm95.dev@gmail.com>

---
+++ src/strategy/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { JwtPayload } from '../interface';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+
+    delete user.hash;
+    return user;
+  }
+}
